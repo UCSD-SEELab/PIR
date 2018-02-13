d13b9a8fc7cad4296df21e011294b4db27d7ac10
Tianchang <gtianchang@gmail.com>
2018-02-13 01:24:30 -0800

    v

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ from datetime import datetime, timedelta
 import pandas
 
 
-def collect_samples(pir,cam,secs_duration, refresh_rate):
+def collect_samples(pir,cam, cycle, refresh_rate):
     print("collecting data...")
-    ref_diff = timedelta(0,secs_duration)
     start_time = datetime.now()
     timestamp = str(start_time)
     pir_values = []
     filenames = []
     index = 0
-    while datetime.now() - start_time < ref_diff:
+    for _ in range(cycle):
         pir_values.extend(pir.read_pir())
 	cam.take_picture(str(index)+".jpg")
         filenames.append(str(index)+".jpg")
@@ -32,12 +31,12 @@ def collect_samples(pir,cam,secs_duration, refresh_rate):
 if __name__ == "__main__":
     pir = pir([23, 24, 25, 27, 22])
     cam = cam()
-    duration = 5
+    cycle = 6
     refresh_rate = 0.3
 
     while True:
         if not np.all(pir.read_pir() == [0, 0, 0, 0, 0]):
-            ans = collect_samples(pir,cam,duration,refresh_rate)
+            ans = collect_samples(pir,cam,cycle,refresh_rate)
             publish_pir_mqtt("pir","living_room/pir","iot.eclipse.org",ans)
 	else:
             time.sleep(refresh_rate)
