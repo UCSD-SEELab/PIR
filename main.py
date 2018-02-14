3e3debd854420f28d5cfdbac4415c0a5c43f5f16
Tianchang <gtianchang@gmail.com>
2018-02-13 21:32:59 -0800

    m

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ if __name__ == "__main__":
             payload = json.dumps(ans)
             client.publish(topic, payload, qos=0, retain=False)
             print("publishing " + payload)
-	    else:
+        else:
             time.sleep(refresh_rate)
             print("idle")
