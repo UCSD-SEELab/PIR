1e62edf6627e164d1c3c313d212d69e18edd9054
Tianchang <gtianchang@gmail.com>
2018-02-14 02:27:28 -0800

    b

--- main_laptop.py
+++ main_laptop.py
@@ -2,12 +2,24 @@
 
 import paho.mqtt.client as mqtt
 import time
-
+import json
+from simplecrypt import encrypt, decrypt
+import pickle
+from sklearn.cluster import KMeans
 
 def on_message(client, userdata, message):
+    fh = open("data_no_cam_laptop_client.txt","a")
+    fh.write(message.payload+"\n")
+    fh.close()
+    #global mqtt_data
+    #mqtt_data = json.loads(message.payload)
     print(message.topic + " " + str(message.payload))
 
 if __name__ == "__main__":
+    with open("kmeans","rb") as sf:
+        kmeans = pickle.load(sf)
+
+    mqtt_data = ''
     name = "laptop"
     topic = "tAxiY7W4P58QH5Oq/living_room/pir"
     broker_address = "iot.eclipse.org"
